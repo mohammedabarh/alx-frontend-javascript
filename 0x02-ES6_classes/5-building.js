bc48b31c164e6f5a49201c715d09ebbb339de670
mohammedabarh <abmohammed606@gmail.com>
2025-04-17 21:43:04 +0000

    task

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,21 @@
-export default class Building {
+class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && 
-        !this.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+    this.sqft = sqft;
+  }
+
+  /**
+   * @param {Number} sqft
+   */
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft nust be a number');
     }
     this._sqft = sqft;
   }
@@ -11,3 +24,5 @@ export default class Building {
     return this._sqft;
   }
 }
+
+export default Building;
